7fb4d93cbbef0766fc43f6e554b854302f03a0af
Thuong Nguyen <70764112+ngocThuong2001@users.noreply.github.com>
2022-02-05 10:21:46 +0700

    Add files via upload

---
+++ Lap3-PhepBienDoi2D/tinh-tien.cpp
@@ -0,0 +1,16 @@
+#include <graphics.h>
+
+int main(){
+	initwindow(1000,1000);
+	int x1 = 100, x2 = 140;
+	int y1 = 150, y2 = 60;
+	
+	for (int i=0; i<100; i++){
+		cleardevice();
+		line(x1+i, y1+i, x2+i, y2+i);
+		Sleep(50);
+	}
+	getch();
+	return 0;
+	
+}
